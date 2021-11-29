c69e1580f64100b1c9518541b5a1b4dc6573ea45
Ubuntu <ubuntu@ip-172-31-19-9.us-east-2.compute.internal>
2021-11-29 13:25:15 +0000

    Finish 6.2.3

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -13,12 +13,22 @@ class UserTest < ActiveSupport::TestCase
   end
   
   test "name should be present" do
-    @user.name = ""
+    @user.name = "  "
     assert_not @user.valid?
   end
   
   test "email should be present" do
-    @user.email = "     "
+    @user.email = "  "
+    assert_not @user.valid?
+  end
+  
+  test "name should not be too long" do
+    @user.name = "a" * 51
+    assert_not @user.valid?
+  end
+  
+  test "email should not be too long" do
+    @user.email = "a" * 244 + "@example.com"
     assert_not @user.valid?
   end
   
