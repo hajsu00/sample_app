fad6b61a12d14e33c8d5f07db61875d7afa05ad1
Ubuntu <ubuntu@ip-172-31-19-9.us-east-2.compute.internal>
2021-12-04 07:34:44 +0000

    Finish 8.2.4

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,7 @@ class SessionsController < ApplicationController
   
   def create
     user = User.find_by(email: params[:session][:email].downcase)
-    if user && user.authenticate(params[:session][:password])
+    if user &.authenticate(params[:session][:password])
       # ユーザーログイン後にユーザー情報のページにリダイレクトする
       log_in user   #'sessions_helper.rb'に書いたヘルパーメソッドを'user'を引数にして呼び出している
       redirect_to user  #参考→https://qiita.com/japwork/items/b84e3fbee5cc6946316c
