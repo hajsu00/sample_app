ac09b5ada17369201dd76bb9ecefb0592f97da09
Ubuntu <ubuntu@ip-172-31-19-9.us-east-2.compute.internal>
2021-12-04 02:59:18 +0000

    Finish 8.2.3

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,11 +6,11 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       # ユーザーログイン後にユーザー情報のページにリダイレクトする
-      log_in user
-      redirect_to user
+      log_in user   #'sessions_helper.rb'に書いたヘルパーメソッドを'user'を引数にして呼び出している
+      redirect_to user  #参考→https://qiita.com/japwork/items/b84e3fbee5cc6946316c
     else
       # エラーメッセージを作成する
-      flash.now[:danger] = 'Invalid email/password combination' # 本当は正しくない
+      flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
     end
   end
