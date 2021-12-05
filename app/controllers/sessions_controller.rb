8747504a2550827417bf86625f86bbf86e9f105b
Ubuntu <ubuntu@ip-172-31-19-9.us-east-2.compute.internal>
2021-12-05 02:31:26 +0000

    Implement advanced login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,12 +3,12 @@ class SessionsController < ApplicationController
   end
   
   def create
-    user = User.find_by(email: params[:session][:email].downcase)
-    if user &.authenticate(params[:session][:password])
+    @user = User.find_by(email: params[:session][:email].downcase)
+    if @user &.authenticate(params[:session][:password])
       # ユーザーログイン後にユーザー情報のページにリダイレクトする
-      log_in user   #'sessions_helper.rb'に書いたヘルパーメソッドを'user'を引数にして呼び出している
-      params[:session][:remember_me] == '1'? remmeber(user) : forget(user)  #三項演算子（ternary operator）
-      redirect_to user  #参考→https://qiita.com/japwork/items/b84e3fbee5cc6946316c
+      log_in @user   #'sessions_helper.rb'に書いたヘルパーメソッドを'user'を引数にして呼び出している
+      params[:session][:remember_me] == '1'? remember(@user) : forget(@user)  #三項演算子（ternary operator）
+      redirect_to @user  #参考→https://qiita.com/japwork/items/b84e3fbee5cc6946316c
     else
       # エラーメッセージを作成する
       flash.now[:danger] = 'Invalid email/password combination'
