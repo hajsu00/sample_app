7ec3692383bf3f593465b59d67c14231c1f1c1c8
Ubuntu <ubuntu@ip-172-31-19-9.us-east-2.compute.internal>
2021-12-07 11:29:32 +0000

    Finish 10.2

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
       # ユーザーログイン後にユーザー情報のページにリダイレクトする
       log_in @user   #'sessions_helper.rb'に書いたヘルパーメソッドを'user'を引数にして呼び出している
       params[:session][:remember_me] == '1'? remember(@user) : forget(@user)  #三項演算子（ternary operator）
-      redirect_to @user  #参考→https://qiita.com/japwork/items/b84e3fbee5cc6946316c
+      redirect_back_or @user
     else
       # エラーメッセージを作成する
       flash.now[:danger] = 'Invalid email/password combination'
--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -37,6 +37,25 @@ class UsersEditTest < ActionDispatch::IntegrationTest
     assert_equal email, @user.email
   end
   
+  test "successful edit with friendly forwarding" do
+    get edit_user_path(@user)
+    assert_equal session[:forwarding_url], edit_user_url(@user)  #<---注目
+    log_in_as(@user)  #sessions_controllerの"create"アクションが自動的に呼び出される
+    assert_nil session[:forwarding_url]   #リダイレクト終了後に"formarding_url"がクリアされているか確認
+    assert_redirected_to edit_user_url(@user)
+    name = "Foo Bar"
+    email = "foo@bar.com"
+    patch user_path(@user), params: { user: { name: name,
+                                              email: email,
+                                              password: "",
+                                              password_confirmation: "" } }
+    assert_not flash.empty?
+    assert_redirected_to @user
+    @user.reload
+    assert_equal name, @user.name
+    assert_equal email, @user.email
+  end
+  
   test "should redirect edit when not logged in" do
     get edit_user_path(@user)
     assert_not flash.empty?
